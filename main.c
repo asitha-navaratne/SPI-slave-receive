59df54d491fa9a85a2a9e00caa1cb870349223a6
Asitha Navaratne <asithan476@gmail.com>
2021-09-29 20:24:23 +0530

    Justified comments

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ int main(void){
 	PORT_INIT();
 	SPI_SLAVE_INIT();
 	
-	unsigned char acknowledge = 0x41;							///< ACK byte.
-	uint8_t datasize = 12;										///< Size of data to be received.
+	unsigned char acknowledge = 0x41;					///< ACK byte.
+	uint8_t datasize = 12;							///< Size of data to be received.
 	
 	char *receive = SPI_RECEIVE_STRING(acknowledge,datasize);
 	
@@ -30,7 +30,7 @@ int main(void){
 
 void PORT_INIT(void){
 	DDRB &= ~((1<<PB4)|(1<<PB5)|(1<<PB7));					///< Set MOSI, SCK and SS pins to input.
-	DDRB |= (1<<PB6);											///< Set MISO pin to output.
+	DDRB |= (1<<PB6);							///< Set MISO pin to output.
 }
 
 /*!
@@ -38,7 +38,7 @@ void PORT_INIT(void){
  */
 
 void SPI_SLAVE_INIT(void){
-	SPCR = (1<<SPE);											///< Enable SPI in Slave SPI mode.
+	SPCR = (1<<SPE);							///< Enable SPI in Slave SPI mode.
 }
 
 /*!
@@ -50,9 +50,9 @@ void SPI_SLAVE_INIT(void){
 char * SPI_RECEIVE_STRING(unsigned char acknowledge,uint8_t datasize){
 	char *message = "";
 	for(uint8_t i=0;i<datasize;i++){
-		SPDR = acknowledge;										///< Load acknowledge byte to SPDR register to shift to master.
-		while(!(SPSR & (1<<SPIF)));							///< Wait until whole data byte is received.
+		SPDR = acknowledge;						///< Load acknowledge byte to SPDR register to shift to master.
+		while(!(SPSR & (1<<SPIF)));					///< Wait until whole data byte is received.
 		message[i] = SPDR;
 	}
-	return message;												///< Return the received string.
+	return message;								///< Return the received string.
 }
